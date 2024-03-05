23d3b7c8aef9dc14affe0e9ab8b21a600be17705
mzZ-66 <pablobello0997@gmail.com>
2024-03-05 14:23:09 +0100

    base

---
+++ DAO/ConexionDB.php
@@ -0,0 +1,21 @@
+<?php
+    class conexionDB {
+        private $conexion;
+        public function __construct() {
+            $this->conexion = new mysqli('localhost', 'root', '', 'examendb');
+            $error = $this->conexion->connect_errno;
+            $errorMessage = $this->conexion->connect_error;
+            if ($error != null) {
+                echo "<p>Error $error conectando a la base de datos: <br> $errorMessage </p><br>";
+            } else {
+                // echo "<p>Conexión realizada con éxito</p><br>";
+            }
+        }
+        public function getConexion() {
+            return $this->conexion;
+        }
+        public function setConexion($conexion) {
+            $this->conexion = $conexion;
+        }
+    }
+?>
